d1889f579260bcddfc8461547d4f489295e7c3ec
Vlad Feninets <v.feninets@gmail.com>
2017-01-03 13:49:58 +0200

    clean

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {Router, Route, IndexRoute, browserHistory} from "react-router"
+
+import Layout from './Pages/Layout'
+import MainPage from './Pages/Main'
+import AboutPage from './Pages/About'
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Router history={browserHistory}>
+        <Route path="/" component={Layout}>
+            <IndexRoute component={MainPage}/>
+            <Route path="/about" component={AboutPage}/>
+        </Route>
+    </Router>,
+    document.getElementById('root')
 );
