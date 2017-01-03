af511d19207440125cfe699d23da7c939d5d36e0
Vlad Feninets <v.feninets@gmail.com>
2017-01-03 15:52:01 +0200

    improve flux

---
+++ src/Constants/actionTypes.js
@@ -0,0 +1,5 @@
+// import keyMirror from 'keymirror'
+//
+// export default actionTypes = () => keyMirror({
+//     GET_POKEMON_LIST
+// })
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,12 +1,52 @@
-import React, {Component} from "react";
+import React, {Component} from "react"
+import AppDispatcher from '../Dispatcher'
+import CircularProgress from 'material-ui/CircularProgress';
+
 
 
 export default class MainPage extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            pokemonsList: null
+        }
+    }
+
+    getPockemons() {
+        fetch('http://pokeapi.co/api/v2/pokemon/?limit=21')
+            .then(response => response.json())
+            .then(data => {
+                AppDispatcher.dispatch({
+                    actionType: 'GET_POKEMONS',
+                    data
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.getPockemons();
+
+        AppDispatcher.register(payload => {
+            if (payload.actionType == 'GET_POKEMONS') {
+                this.setState({pokemonsList: payload.data})
+            }
+        })
+    }
+
     render() {
+
+        const {pokemonsList} = this.state;
+
         return (
-            <div>
-                <br/><br/>
-                MainPage
+            <div style={{margin: "150px 100px"}}>
+                { !pokemonsList
+                    ? <div><CircularProgress /> <br/> FETCH POKEMONS LIST</div>
+                    : <ol> { pokemonsList.results.map(pokemon => {
+                        return <li key={pokemon.name}>{pokemon.name}</li>
+                    }) } </ol>
+                }
             </div>
         );
     }
